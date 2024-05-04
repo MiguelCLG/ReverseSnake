1504d7dc9642d7e1338e6d5f04a483e94744050e
JLFFernandes <101908290+JLFFernandes@users.noreply.github.com>
2024-05-04 17:33:05 +0200

    Adicionei MVC Player
    Adicionei MVC e adicionei movimentos.

---
+++ Assets/Scripts/Player/PlayerController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PlayerController: MonoBehaviour
+{
+    private static PlayerController instance;
+    private PlayerModel model;
+    private PlayerView view;
+
+    private void Awake() {
+        if (instance == null) {instance = this; }
+        else Destroy(this);
+    }
+
+    public void Start()
+    {
+        model = GetComponent<PlayerModel>();
+        view = GetComponent<PlayerView>();
+
+    }
+
+    public void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.A) || Input.GetKeyDown(KeyCode.LeftArrow))
+            MovePlayer(Vector2.left);
+        if (Input.GetKeyDown(KeyCode.S) || Input.GetKeyDown(KeyCode.DownArrow))
+            MovePlayer(Vector2.down);
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+            MovePlayer(Vector2.up);
+        if (Input.GetKeyDown(KeyCode.D) || Input.GetKeyDown(KeyCode.RightArrow))
+            MovePlayer(Vector2.right);
+
+    }
+
+    public void MovePlayer(Vector2 direction)
+    {
+        // Movimenta o jogador e atualiza a visualização
+        model.Move(direction);
+    }
+
+    public void PlayerEatsFood(int scoreValue)
+    {
+        // Adiciona pontos e atualiza a visualização
+        model.AddScore(scoreValue);
+        // view.DisplayPlayer();
+    }
+
+    public void PlayerDies()
+    {
+        // Atualiza o estado para morto e atualiza a visualização
+        model.Die();
+        view.DisplayDeath();
+    }
+}
