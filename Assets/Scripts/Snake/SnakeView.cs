32818b83f288d2878e9cf3ba898ee25632b8c87a
dirop1 <dirop1>
2024-05-26 18:59:53 +0100

    Added snake head rotation

--- Assets/Scripts/Snake/SnakeView.cs
+++ Assets/Scripts/Snake/SnakeView.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class SnakeView : MonoBehaviour, IView
 {
@@ -31,7 +32,15 @@ public class SnakeView : MonoBehaviour, IView
         var previousPosition = newPosition;
         var snakeBodyPositions = new LinkedList<Vector2>();
         var grid = GameGrid.getInstance();
-        foreach (Transform child in transform)
+
+        if(transform.childCount > 0) // caso ja haja snake head
+        {
+            // calcular e aplicar a nova rotação da snake head
+            float angleInDegrees = calculateHeadRotation(grid.CalculateGridCoordinates(transform.GetChild(0).position), newPosition);
+            transform.GetChild(0).rotation = Quaternion.Euler(0, 0, angleInDegrees);
+        }
+
+        foreach (Transform child in transform) // Mover todas as partes da snake para a posicao da anterior parte
         {
             var aux = grid.CalculateGridCoordinates(child.position);
             child.position = grid.CalculateMapPosition(previousPosition);
@@ -39,5 +48,23 @@ public class SnakeView : MonoBehaviour, IView
             previousPosition = aux;
         }
         model.snakeBodyPositions = snakeBodyPositions;
+    }
+
+
+    // Metodo que recebe a nova e a posicao anterior
+    // E retorna o novo angulo para que a snake esteja a olhar para onde vai!
+    private float calculateHeadRotation(Vector2 oldPosition, Vector2 newPosition)
+    {
+        // Calculate the differences in coordinates
+        float deltaX = newPosition.x - oldPosition.x;
+        float deltaY = newPosition.y - oldPosition.y;
+
+        // Calculate the angle in radians
+        float angleInRadians = Mathf.Atan2(deltaY, deltaX);
+
+        // Convert the angle to degrees
+        float angleInDegrees = angleInRadians * Mathf.Rad2Deg;
+
+        return angleInDegrees;
     }
 }
